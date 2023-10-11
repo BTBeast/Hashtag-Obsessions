c21e86217be0d1b8b9591c4803e021938ea2e686
BTBeast <139731926+BTBeast@users.noreply.github.com>
2023-10-11 23:07:31 +0800

    Add files via upload

--- OutdoorHashtags.py
+++ OutdoorHashtags.py
@@ -7,12 +7,13 @@ from collections import Counter
 import leidenalg as la
 import igraph as ig
 from pyvis.network import Network
-import random
+import requests  # Import the requests library
+import matplotlib.cm as cm
+import matplotlib
 
-# Load the JSON data
-file_path = 'processed_instagram_posts.json'
-with open(file_path, 'r', encoding='utf-8') as file:
-    data = json.load(file)
+# Load the JSON data from the URL
+url = 'https://raw.githubusercontent.com/BTBeast/Hashtag-Obsessions/main/processed_instagram_posts.json'
+data = json.loads(requests.get(url).text)  # Load data using requests and convert it to JSON
 
 # Convert the data into a pandas DataFrame
 df = pd.DataFrame(data)
@@ -22,9 +23,9 @@ df['hashtags'] = df['hashtags'].apply(lambda x: x if isinstance(x, list) else []
 all_hashtags = [hashtag for sublist in df['hashtags'] for hashtag in sublist if isinstance(sublist, list)]
 
 # Getting the top 100 frequent hashtags
-top_hashtags = [hashtag for hashtag, count in Counter(all_hashtags).most_common(100)]
+top_hashtags = [hashtag for hashtag, count in Counter(all_hashtags).most_common(1000)]
 
-# Randomly sample 100 posts
+# Randomly sample a number of posts
 sampled_df = df.sample(n=10000, random_state=1)
 
 # Extracting co-occurring top hashtags pairs from the sampled posts
@@ -57,25 +58,41 @@ for node, community in zip(G.nodes(), partition.membership):
 # Creating an interactive network graph using pyvis
 nt = Network(notebook=True, height="750px", width="100%")
 
-# Getting the top 20 hashtags by degree for display
-top_20_hashtags = sorted(G.nodes(), key=lambda x: G.degree(x), reverse=True)[:50]
+# Getting the top 50 hashtags by degree for display
+top_50_hashtags = sorted(G.nodes(), key=lambda x: G.degree(x), reverse=True)[:50]
 
 # Adding labels to the nodes and setting node size based on degree and color based on community
+cmap = cm.Set2  # Updated to use the attribute instead of the function
+
+# Calculate the minimum and maximum degree in the graph
+min_degree = min(dict(G.degree()).values())
+max_degree = max(dict(G.degree()).values())
+
+# Function to normalize the degree values
+def normalize_degree(degree, min_degree, max_degree, new_min, new_max):
+    return ((degree - min_degree) / (max_degree - min_degree)) * (new_max - new_min) + new_min
+
+# Adding labels to the nodes and setting node size based on normalized degree and color based on community
 for node in G.nodes():
-    if node in top_20_hashtags:
+    if node in top_50_hashtags:
         community = G.nodes[node]['community']
         degree = G.degree(node)
+        normalized_degree = normalize_degree(degree, min_degree, max_degree, 5, 100)  # Normalize degree to the range of 5 to 100
+        color = cmap(community / len(set(partition.membership)))  # Normalizing community number to [0,1] for color mapping
+        color = list(color[:3])  # Convert RGB to list
+        color.append(0.9)  # Append alpha channel, 0.9 for half transparency
+        color = matplotlib.colors.rgb2hex(color, keep_alpha=True)  # Convert RGBA to hex
         nt.add_node(node, 
                     label=node, 
                     title=f'{node}\nConnections: {degree}', 
-                    size=degree * 2,  
-                    color=f'rgba({community*40}, {community*80}, {community*120}, 0.8)',
-                    font={"size": degree * 2, "strokeWidth": 0, "color": "white"},  
+                    size=normalized_degree,  # Updated to use normalized degree
+                    color=color,  # Updated color with transparency
+                    font={"size": normalized_degree, "strokeWidth": 0, "color": "white"},  
                     shape='circle')  
 
 # Adding edges
 for edge in G.edges(data=True):
-    if edge[0] in top_20_hashtags and edge[1] in top_20_hashtags:
+    if edge[0] in top_50_hashtags and edge[1] in top_50_hashtags:
         nt.add_edge(edge[0], edge[1], value=edge[2]['weight'])
 
 # Adjusting the physics layout of the network to bring nodes of the same community closer together and reduce jittering
@@ -98,5 +115,5 @@ nt.set_options("""
 }
 """.replace("'", '"'))  
 
-# Displaying the interactive network graph
-nt.show("network_outdoor.html")
+# Save the interactive network graph as an HTML file
+nt.save_graph("network_outdoor.html")
